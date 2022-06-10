a58188c42345f101915bd271a3b573f36b70b303
vasiliev <yuliivasiliev@gmail.com>
2022-06-10 22:53:43 +0300

    First form created

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
+domain = '0.0.0.0'
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['post', 'get'])
+def main_form():
+    if request.method == 'POST':
+        return request.form
+    return render_template("MainForm.html")
 
 
 if __name__ == '__main__':
